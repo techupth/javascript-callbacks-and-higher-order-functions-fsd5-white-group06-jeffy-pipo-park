5fd0b18b9af388a3f8ac95b2f5c071ce6d77db6d
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-05 21:13:51 +0700

    feat(exercise): add completed exercise

--- ex-2.js
+++ ex-2.js
@@ -4,10 +4,6 @@ const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
-
 function atLeastFive(array, operation) {
   let count = 0;
   for (let i = 0; i < array.length; i++) {
@@ -19,12 +15,12 @@ function atLeastFive(array, operation) {
 }
 
 function filter(score) {
-  return score >= 70;
+  return score > 70;
 }
 
-scoreRoom1Result = atLeastFive(studentScoresRoom1, filter);
-scoreRoom2Result = atLeastFive(studentScoresRoom2, filter);
-scoreRoom3Result = atLeastFive(studentScoresRoom3, filter);
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, filter);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, filter);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, filter);
 
 console.log(scoreRoom1Result);
 console.log(scoreRoom2Result);
