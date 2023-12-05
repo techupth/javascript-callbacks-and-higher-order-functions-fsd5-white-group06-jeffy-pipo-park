d7a138d3954949b38ed08bb325a5ad04ded86de8
Park <tanagrit.sornchaiyagorn@gmail.com>
2023-12-05 15:26:01 +0700

    ADD: Add callback function to increase flexibility in ex1-2 files

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,19 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
+  operation(array);
+}
+
+function add5000(array) {
+  for (let i of employeeSalaries) {
+    console.log(i);
+    array.push(i + 5000);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
+forEach(newEmployeeSalaries, add5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,33 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  return operation(array);
 }
-
+function checkScoreOver70(array) {
+  let countOver70 = 0;
+  //count over 70
+  for (let i of array) {
+    if (i >= 70) {
+      countOver70++;
+    }
+  }
+  // check count 5 or Over
+  if (countOver70 >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScoreOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScoreOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScoreOver70);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
