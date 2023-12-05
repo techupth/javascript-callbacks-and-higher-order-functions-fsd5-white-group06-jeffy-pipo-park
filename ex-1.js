51437fa82ea37304d38de247b2caed9e22e37e2f
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-05 21:05:35 +0700

    feat(exercise): add completed exercise

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,18 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
+function forEach(array, operation) {
+  for (let i = 0; i < array.length; i++) {
+    operation(array[i]);
+  }
+}
+
+function add5000(salary) {
+  newEmployeeSalaries.push(salary + 5000);
+}
+
+forEach(employeeSalaries, add5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,31 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
-  // Start coding here
-}
-
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
-
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+function atLeastFive(array, operation) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      count++;
+    }
+  }
+  return count >= 5;
+}
+
+function filter(score) {
+  return score >= 70;
+}
+
+scoreRoom1Result = atLeastFive(studentScoresRoom1, filter);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, filter);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, filter);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
